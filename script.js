0843a38b7011349220035e4402114831dd7b0ee8
JeromePrevotat <jer.prevotat@gmail.com>
2025-07-16 12:05:08 +0200

    adress lookup

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 // import { BornePrivee } from './modules/privateBorne.js';
 // import { Reservation } from './modules/reservation.js';
 
+const adresseInput = document.getElementById("adresse-input");
+const adressLookupButton = document.getElementById("adress-lookup-btn");
 const mapContainer = document.getElementById("map");
 let map = L.map(mapContainer);
 // let positionMarker = L.marker();
+// positionMarker = positionMarker.setLatLng([latitude, longitude]).addTo(map);
 
 function addMapCredits(map){
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,8 +24,7 @@ function getLocation(){
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         map = map.setView([latitude, longitude], 14);
-        // positionMarker = positionMarker.setLatLng([latitude, longitude]).addTo(map);
-        console.log(`Latitude: ${latitude}, Longitude: ${longitude}, positionAccuracy: ${position.coords.accuracy}`);
+        // console.log(`Latitude: ${latitude}, Longitude: ${longitude}, positionAccuracy: ${position.coords.accuracy}`);
     },
     (error) => {
         console.error("Error getting location:", error);
@@ -31,20 +33,63 @@ function getLocation(){
     { timeout: 5000, enableHighAccuracy: true, maximumAge: 2000 });
 }
 
-function initMap(mapContainer) {
+function initMap() {
     getLocation();
     addMapCredits(map);
     console.log("Map initialized");
 }
 
+async function adressLookUp(){
+    const endpoint = 'https://nominatim.openstreetmap.org/search?q=';
+    const queryParam = sanitizeInput(adresseInput.value) + '&format=json';
+
+    try {
+        let response = await fetch(endpoint + queryParam, {
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Evaluation JavaScript App'
+            }
+        });
+        // Error handling
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    
+        const data = await response.json();
+        console.log("Response from Nominatim:", data);
+        return data;
+    }
+    catch (error) {
+        console.error("Error fetching data from Nominatim:", error);
+    }
+}
+
 function addEventListeners() {
+    const testButton = document.getElementById("test-button");
+    testButton.addEventListener("click", () => test());
+
+    adressLookupButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        adressLookUp();
+    });
     console.log("Event Listeners added");
+}
+
+function sanitizeInput(input) {
+    // Removes <HTML Tags> and trims whitespace
+    return input.replace(/<[^>]*>/g, '').trim();
 }
 
 function main(){
     console.log("Electricity Business Application Started");
     addEventListeners();
     initMap('map');
+}
+
+
+
+
+
+async function test(){
+    
 }
 
 main();
